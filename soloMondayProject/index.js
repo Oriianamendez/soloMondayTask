ea08f17106d2fc312548226d74e2ac1172df6420
Oriana Mendez <egleemendez@gmail.com>
2024-06-24 16:22:01 +0200

    console log function

--- soloMondayProject/index.js
+++ soloMondayProject/index.js
@@ -5,6 +5,6 @@ function getCountry(response) {
   const roundTemp = Math.round(celsiusTemp);
 
   const apiUrl = `http://api.weatherapi.com/v1/current.json?key=722bcc523c5840e6a07113521242406&q=stockholm`;
-  console.log(axios.get(apiUrl).then(celsiusTemp));
+  axios.get(apiUrl).then(celsiusTemp);
 }
 console.log(getCountry());
